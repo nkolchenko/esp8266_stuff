a51d6fd2b86fcc868fc64ed4e4998560c0f4d12e
nkolchenko <nikolay.kolchenko@strikead.com>
2018-01-14 18:34:29 +0100

    I did autoformat plus minor changes

--- sketch_knp_default_2.ino
+++ sketch_knp_default_2.ino
@@ -1,24 +1,18 @@
 #include <LiquidCrystal_I2C.h>
-
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 
 ESP8266WiFiMulti WiFiMulti;
 LiquidCrystal_I2C lcd(0x3F, 20, 4);
 
-
-
 void setup() {
-    // The begin call takes the width and height. This
-  // Should match the number provided to the constructor.
+
   lcd.begin();
-  //lcd.init();
 
   // Turn on the backlight.
   lcd.backlight();
 
-  // Move the cursor characters to the right and
-  // zero characters down (line 1).
+  // Move the cursor 5 characters to the right and zero characters down (line 1).
   lcd.setCursor(5, 0);
 
   // Print HELLO to the screen, starting at 5,0.
@@ -26,39 +20,38 @@ void setup() {
 
   // Move the cursor to the next line and print
   // WORLD.
-  lcd.setCursor(5, 1);      
+  lcd.setCursor(5, 1);
   lcd.print("WORLD");
-    pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-    Serial.begin(74880);
-    delay(10);
 
-    // We start by connecting to a WiFi network
-    WiFi.mode(WIFI_STA);
-    WiFiMulti.addAP("r2d2-ng", "tcmrjdf!wifi");
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  Serial.begin(74880);
+  delay(10);
 
-    Serial.println();
-    Serial.println();
-    Serial.print("Looking for a WiFi: ");
+  // We start by connecting to a WiFi network
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("r2d2-ng", "tcmrjdf!wifi");
 
-    while(WiFiMulti.run() != WL_CONNECTED) {
-        Serial.print(".");
-        delay(500);
-    }
+  Serial.println();
+  Serial.println();
+  Serial.print("Looking for a WiFi: ");
 
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    lcd.setCursor(2, 3);      
-    lcd.print(WiFi.localIP());
+  while (WiFiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
     delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  lcd.setCursor(2, 3);
+  lcd.print(WiFi.localIP());
+  delay(500);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-                                    // but actually the LED is on; this is because 
-                                    // it is active low on the ESP-01)
-  delay(1200);                      // Wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(800);                      // Wait for two seconds (to demonstrate the active low LED)
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED off
+  delay(1200);                      // Wait for a 1.2 seconds
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED on by making the voltage HIGH
+  delay(800);                      // Wait for two seconds
 }
